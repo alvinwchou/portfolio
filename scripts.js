292db765023fa7b8ac5ae5baa166e9c1d0d6d437
Alvin Chou <alvinchou516@hotmail.com>
2022-03-05 23:28:14 -0500

    add eventlistener for bg img change on mouseover

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 const portfolio = {};
 
+portfolio.porjectBackgroundImage = {
+    project1: `url('../assets/pokemonxwordle-background2dull.png')`,
+    project2: `url('../assets/sanctuary-helpers-homedull.jpg')`,
+    project3: `url('../assets/project-bg2.jpg')`,
+    project4: `url('../assets/project-bg.jpg')`,
+}
+
 portfolio.eventListenerSetUp = () => {
     // close slide out nav
     document.querySelector('.slideOutNav button').addEventListener('click', () => {
@@ -7,7 +14,26 @@ portfolio.eventListenerSetUp = () => {
     });
 
     // display description of project
-    document.querySelector
+    const projects = document.querySelectorAll('.projectBox');
+    projects.forEach( (project) => {
+        console.log(project);
+        project.querySelector('a').addEventListener('mouseover', (e) => {
+            // console.log("mouse");
+            console.log(e.target);
+
+            if (e.target.localName == 'a') {
+                document.querySelector('.projectDisplay').innerHTML = e.target.parentElement.innerHTML
+            } else if (e.target.parentElement.localName == 'a') {
+                console.log('!!!!!!!!!!!!!!!!!!you are on the right track!!!!!!!!!!!!!!!!!!!');
+                console.log(e.target.parentElement.parentElement.classList[1]);
+                console.log(portfolio.porjectBackgroundImage[e.target.parentElement.parentElement.classList[1]]);
+                document.querySelector('.myProjects').style.backgroundImage = portfolio.porjectBackgroundImage[e.target.parentElement.parentElement.classList[1]];
+
+                document.querySelector('.projectDisplay').innerHTML = e.target.parentElement.parentElement.innerHTML;
+            }
+
+        });
+    });
 };
 
 portfolio.init = () => {
