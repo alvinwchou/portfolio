19b699b7e55e96daf91a5087e19572c3bced2702
Alvin Chou <alvinchou516@hotmail.com>
2022-03-06 19:33:40 -0500

    finally fixed the myProject bg with transition

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
 const portfolio = {};
 
-portfolio.porjectBackgroundImage = {
-    project1: `url('../assets/pokemonxwordle-background2dull.png')`,
-    project2: `url('../assets/sanctuary-helpers-homedull.jpg')`,
-    project3: `url('../assets/project-bg2.jpg')`,
-    project4: `url('../assets/project-bg.jpg')`,
+// portfolio.porjectBackgroundImage = {
+//     project1: `url('../assets/pokemonxwordle-background2dull.png')`,
+//     project2: `url('../assets/sanctuary-helpers-homedull.jpg')`,
+//     project3: `url('../assets/project-bg2.jpg')`,
+//     project4: `url('../assets/project-bg.jpg')`,
+// }
+
+portfolio.backgroundChanager = (number) => {
+    const projectBackgrounds = document.querySelectorAll('.projectBackground')
+    projectBackgrounds.forEach( (projectBackground) => {
+        projectBackground.classList.remove('zoom')
+        console.log('removed');
+    })
+    projectBackgrounds[number].classList.add('zoom')
 }
+
 
 portfolio.eventListenerSetUp = () => {
     // close slide out nav
@@ -15,26 +25,47 @@ portfolio.eventListenerSetUp = () => {
 
     // display description of project
     const projects = document.querySelectorAll('.projectBox');
-    projects.forEach( (project) => {
+    projects.forEach( (project, index) => {
         console.log(project);
         project.querySelector('a').addEventListener('mouseover', (e) => {
             // console.log("mouse");
-            console.log(e.target);
+            // console.log(e.target);
+            // project.addEventListener('mouseover', () => {
+            //     console.log(project.textContent, index);
+            // })
 
+            // const projectBackgrounds = document.querySelectorAll('.projectBackground');
             if (e.target.localName == 'a') {
                 document.querySelector('.projectDisplay').innerHTML = e.target.parentElement.innerHTML
-                console.log('>>>>>>>>', e.target);
+
+                // console.log(document.querySelectorAll('.projectBackground'))
+
+
+                // console.log(projectBackgrounds[index]);
+
+                // projectBackgrounds.forEach( (projectBackground) => {
+                //     projectBackground.classList.remove('zoom')
+                //     console.log('removed');
+                // })
+
+                // projectBackgrounds[index].classList.add('zoom')
+                portfolio.backgroundChanager(index);
+
             } else if (e.target.parentElement.localName == 'a') {
-                // console.log('!!!!!!!!!!!!!!!!!!you are on the right track!!!!!!!!!!!!!!!!!!!');
-                // console.log(e.target.parentElement.parentElement.classList[1]);
-                // console.log(portfolio.porjectBackgroundImage[e.target.parentElement.parentElement.classList[1]]);
-                // document.querySelector('.myProjects').style.backgroundImage = portfolio.porjectBackgroundImage[e.target.parentElement.parentElement.classList[1]];
-                document.querySelector('.projectBackground').style.backgroundImage = portfolio.porjectBackgroundImage[e.target.parentElement.parentElement.classList[1]];
-                document.querySelector('.projectBackground').classList.remove('zoom');
-                document.querySelector('.projectBackground').classList.add('zoom');
+                // document.querySelector('.projectBackground').style.backgroundImage = portfolio.porjectBackgroundImage[e.target.parentElement.parentElement.classList[1]];
+                // document.querySelector('.projectBackground').classList.remove('zoom');
+                // document.querySelector('.projectBackground').classList.add('zoom');
 
 
                 document.querySelector('.projectDisplay').innerHTML = e.target.parentElement.parentElement.innerHTML;
+
+                // projectBackgrounds.forEach( (projectBackground) => {
+                //     projectBackground.classList.remove('zoom')
+                //     console.log('removed');
+                // })
+                // projectBackgrounds[index].classList.add('zoom')
+                portfolio.backgroundChanager(index);
+                
             }
 
         });
